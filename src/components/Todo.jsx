3175b94e12b6a9df497612f795d4f1c9cec963bf
ikhwannaufal <ikhwannaufal@gmail.com>
2024-04-01 22:35:41 +0800

    update todo title

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -31,7 +31,7 @@ const Todo = () => {
   return (
     <div className="max-w-4xl mx-auto sm:mt-8 p-4 bg-gray-100 rounded">
       <h2 className="mt-3 mb-6 text-2xl font-bold text-center uppercase">
-        WORKOUT APP
+        TODO APP
       </h2>
 
       {/* input text and button */}
@@ -59,7 +59,7 @@ const Todo = () => {
           <input
             className="flex-grow p-2 border-b-2 border-gray-300 focus:outline-none focus:border-blue-500"
             type="text"
-            placeholder="Search Workout"
+            placeholder="Search Todo"
             value={searchTerm}
             onChange={(e) => handleSearchChange(e.target.value)}
           />
